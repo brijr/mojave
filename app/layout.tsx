c6bc9db730bb1a8100f787f4137bb3356e2f2560
Bridger Tower <bridgertower@gmail.com>
2024-08-19 11:44:30 -0600

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <Layout>
+    <Layout className="bg-stone-200">
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -2,30 +2,47 @@ import Image from "next/image";
 import Link from "next/link";
 import portfolioRegistry from "@/lib/registry";
 import { PortfolioItem } from "@/lib/registry";
+import { Link2Icon } from "@radix-ui/react-icons";
 
 export function Portfolio() {
   return (
     <div className="grid md:grid-cols-3 gap-4 not-prose">
-      {portfolioRegistry.items.map((item) => (
-        <PortfolioImage item={item} />
+      {portfolioRegistry.items.map((item, index) => (
+        <PortfolioImage
+          key={item.id}
+          item={item}
+          delay={index === 0 ? 500 : 500 + 150 * index}
+        />
       ))}
     </div>
   );
 }
 
-const PortfolioImage = ({ item }: { item: PortfolioItem }) => {
+const PortfolioImage = ({
+  item,
+  delay,
+}: {
+  item: PortfolioItem;
+  delay?: number;
+}) => {
   return (
-    <Link href={item.url} key={item.id}>
+    <div style={{ animationDelay: `${delay}ms` }} className="fade-in-bottom">
       <Image
         src={item.url}
         alt={item.name}
         width={1080}
         height={1080}
-        className="border"
+        className="border rounded"
         placeholder="blur"
         blurDataURL={item.url}
       />
-      <span className="text-sm">{item.name}</span>
-    </Link>
+      <div className="flex gap-2 mt-2 items-center justify-between">
+        <p className="text-sm">{item.name}</p>
+        <Link href={item.url}>
+          <Link2Icon />
+          <span className="sr-only">Link to {item.name}</span>
+        </Link>
+      </div>
+    </div>
   );
 };
